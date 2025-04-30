0667d3f128dc90c708a54185a144f731d1d4535b
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2025-04-30 02:53:54 -0400

    Fix image paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['mixed-decls'],
-      },
-    },
-  },
+  plugins: [react()]
 })
